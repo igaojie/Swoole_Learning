94246da019202d10cfb24d61c5a0774e1d0a23cb
ShaoGaoJie <shaogaojie@gmail.com>
2018-01-13 16:11:21 +0800

    tcp.php

--- tcp.php
+++ tcp.php
@@ -10,7 +10,7 @@ $host = '0.0.0.0';
 $port = 9501;
 //可以不写 默认
 $mode = SWOOLE_PROCESS;//多进程方式
-$host = SWOOLE_SOCK_TCP;
+$sock_type = SWOOLE_SOCK_TCP;
 
 $server = new swoole_server($host, $port, $mode, $sock_type);
 
@@ -23,14 +23,51 @@ $server = new swoole_server($host, $port, $mode, $sock_type);
  *          close: 当关闭连接
  *
  */
+/**
+ * $server 服务器信息
+ * $fd 客户端信息
+ */
 $server->on('connect', function ($server, $fd) {
     echo "connection open: {$fd}\n";
 });
+
+/**
+ * $reactor_id
+ * $data 数据
+ */
 $server->on('receive', function ($server, $fd, $reactor_id, $data) {
+    echo "receive data:{$data}";
     $server->send($fd, "Swoole: {$data}");
     $server->close($fd);
 });
+
 $server->on('close', function ($server, $fd) {
     echo "connection close: {$fd}\n";
 });
+
 $server->start();
+
+
+/*
+ *
+ * 启动tcp服务
+ * ➜  Swoole_Learning git:(master) ✗ /usr/local/Cellar/php72/7.2.1_12/bin/php tcp.php
+ *
+ * ➜  ~ git:(master) ✗ pstree  | grep tcp | grep -v grep
+ | |     \-+= 29592 ShaoGaoJie /usr/local/Cellar/php72/7.2.1_12/bin/php tcp.php
+ | |       \-+- 29593 ShaoGaoJie /usr/local/Cellar/php72/7.2.1_12/bin/php tcp.php
+ | |         |--- 29594 ShaoGaoJie /usr/local/Cellar/php72/7.2.1_12/bin/php tcp.php
+ | |         |--- 29595 ShaoGaoJie /usr/local/Cellar/php72/7.2.1_12/bin/php tcp.php
+ | |         |--- 29596 ShaoGaoJie /usr/local/Cellar/php72/7.2.1_12/bin/php tcp.php
+ | |         \--- 29597 ShaoGaoJie /usr/local/Cellar/php72/7.2.1_12/bin/php tcp.php
+
+
+
+➜  ~ git:(master) ✗ telnet 127.0.0.1 9501
+Trying 127.0.0.1...
+Connected to localhost.
+Escape character is '^]'.
+swoole test
+Swoole: swoole test
+Connection closed by foreign host.
+*/
