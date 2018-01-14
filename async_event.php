e4a04fed86bd051dbf57c9ab2df91fa5a9ae02fa
ShaoGaoJie <shaogaojie@gmail.com>
2018-01-14 12:41:13 +0800

    异步时间

--- async_event.php
+++ async_event.php
@@ -5,3 +5,18 @@
  * Date: 2018/1/14
  * Time: 下午12:22
  */
+$fp = stream_socket_client("tcp://www.qq.com:80",$errno,$errstr,30);
+fwrite($fp,"GET / HTTP/1.1 \r\n Host: www.qq.com \r\n\r\n");
+
+swoole_event_add($fp,function ($fp){
+    $resp = fread($fp,8192);
+
+    echo $resp;
+
+    swoole_event_del($fp);
+
+    fclose($fp);
+});
+
+
+echo "这个先执行完成 \n";
